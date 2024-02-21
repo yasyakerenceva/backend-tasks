e7ac47f58fdd8518672f2ced9c1448b3d6b7fd3e
Yaroslava Kerenceva <pyi.work@yandex.ru>
2024-02-21 10:49:07 +0700

    add hometask-1

---
+++ hometask-1/index.js
@@ -0,0 +1,42 @@
+const yargs = require("yargs");
+const { addNote, removeNote, printNotes } = require("./notes.controller");
+
+yargs.command({
+	command: "add",
+	describe: "Add new note to list",
+	builder: {
+		title: {
+			type: "string",
+			describe: "Note title",
+			demandOption: true,
+		},
+	},
+	handler({ title }) {
+		addNote(title);
+	},
+});
+
+yargs.command({
+	command: "list",
+	describe: "Print all notes",
+	handler() {
+		printNotes();
+	},
+});
+
+yargs.command({
+	command: "remove",
+	describe: "Remove note by ID",
+	builder: {
+		id: {
+			type: "string",
+			describe: "Note ID",
+			demandOption: true,
+		},
+	},
+	handler({ id }) {
+		removeNote(id);
+	},
+});
+
+yargs.parse();
---
+++ hometask-1/notes.controller.js
@@ -0,0 +1,58 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+	const notes = await getNotes();
+
+	const note = {
+		title,
+		id: Date.now().toString(),
+	};
+
+	notes.push(note);
+
+	await fs.writeFile(notesPath, JSON.stringify(notes));
+	console.log(chalk.blue.inverse("Note was added!"));
+}
+
+async function getNotes() {
+	const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+	const notesParse = JSON.parse(notes);
+
+	return Array.isArray(notesParse) ? notesParse : [];
+}
+
+async function removeNote(id) {
+	const notes = await getNotes();
+	const filteredNotes = notes.filter((note) => note.id !== id);
+
+	if (filteredNotes.length !== notes.length) {
+		await fs.writeFile(notesPath, JSON.stringify(filteredNotes));
+		console.log(chalk.red.bold("Note was removed!"));
+	} else {
+		console.log(chalk.bgGrey.inverse("Note does not exist with this id!"));
+	}
+}
+
+async function printNotes() {
+	const notes = await getNotes();
+
+	if (!notes.length) {
+		console.log(chalk.bgRed.bold("No notes!"));
+	} else {
+		console.log(chalk.bgGreen.bold("Here is the list of notes:"));
+
+		notes.forEach((note) => {
+			console.log(chalk.green(note.id, note.title));
+		});
+	}
+}
+
+module.exports = {
+	addNote,
+	removeNote,
+	printNotes,
+};
